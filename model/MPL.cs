2149137ee97865b5afad89a1b2a40f1ad121f992
Mehrdad Safaeikhour <m.safaeikhour@tu-bs.de>
2018-11-07 00:35:32 +0100

    commit

--- model/MPL.cs
+++ model/MPL.cs
@@ -2,13 +2,19 @@ SYNTAXDEF mpl
 FOR <http://mdsd.edu/mpl/1.0>
 START Program
 
+
 OPTIONS {
 	reloadGeneratorModel = "true";
+	defaultTokenName= "IDENTIFIER_TOKEN";
 }
 
 TOKENS{
+DEFINE IDENTIFIER_TOKEN $ ('a'..'z'|'A'..'Z'|'_') ('a'..'z'|'A'..'Z'|'_'|'0'..'9')*$;
 DEFINE INTEGER_TOKEN $('-')?('0'..'9')+$;
+DEFINE SL_COMMENT $'//'(~('\n'|'\r'|'\uffff'))*$;
+DEFINE ML_COMMENT  $'/*'.*'*/'$;
 }
+
 
 RULES {
 Program::="Program" name[] (variabledeclarations ("," variabledeclarations)* ".")? (statement)* "End" ".";
